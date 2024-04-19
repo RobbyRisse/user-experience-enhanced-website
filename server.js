7e3a187c4dd76ecca23b708644d18d1895b42f08
RobbyRisse <robby.risse@gmail.com>
2024-04-19 12:23:56 +0200

    push

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log('Hier komt je server voor Sprint 10. Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+
+// Stel de map met ejs templates in
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static('public'))
+
+// Maak een GET route voor de index
+app.get('/', function (request, response) {
+  // Haal alle SDGs uit de directus API op
+  fetchJson("https://fdnd-agency.directus.app/items/hf_sdgs").then((apiData) => {
+    // apiData bevat gegevens van alle sdg's
+    // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
+
+    // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd sdgs
+    response.render('index', {sdgs: apiData.data })
+  })
+})
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8001)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  // console.log(`Application started on http://localhost:${app.get('port')}`)
+})
